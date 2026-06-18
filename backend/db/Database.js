3bdedb98579e5a789c09a5e94f249c0197d214bf
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-18 23:55:59 +0500

    Update Database.js

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,29 +1,8 @@
-const mongoose = require("mongoose");
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
-const connectDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.DB_URL).then((mongoose) => {
-      console.log("Connected DB:", mongoose.connection.name);
-      console.log("Connected Host:", mongoose.connection.host);
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+const connectDatabase = () => {
+  mongoose.connect(process.env.DB_URL).then((data) => {
+    console.log(
+      `Mongo db connected with server:${data.connection.host} successfully `
+    );
+  });
 };
-
 module.exports = connectDatabase;
