d6c846c0ddd5d61b7abae8ebb02acf5440a6fd9c
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-18 11:56:05 +0500

    Update sendShopToken.js

--- backend/utils/sendShopToken.js
+++ backend/utils/sendShopToken.js
@@ -1,11 +1,12 @@
 
 const shopToken=async (user , statusCode , res)=>{
     const token=user.getJwtToken();
+     const isProd = process.env.NODE_ENV === "production";
     const options={
         expires : new Date(Date.now()+90*24*60*60*1000),
         httpOnly:true,
-        sameSite: "lax", // Critical for cross-origin cookies
-        secure: false,    // Required if sameSite is "none"
+    sameSite: isProd ? "none" : "lax",
+    secure: isProd,
     }
     res.status(statusCode).cookie("seller_token" , token , options).json({
         success:true , 
