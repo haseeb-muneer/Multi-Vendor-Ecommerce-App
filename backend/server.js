4bc93ba71d00e07e1df42091a6a14616117fdcbf
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-19 00:05:18 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,5 @@
 const app = require("./app");
-const cloudinary = require("cloudinary");
 
-process.on("uncaughtException", (err) => {
-  console.log(`Error : ${err.message}`);
-  console.log("Shutting down the server for handling uncaught exception");
-});
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "config/.env" });
-}
-
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 8000}`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log(`shutting down the server for handling ${err.message}`);
-
-  server.close(() => {
-    process.exit(1);
-  });
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
