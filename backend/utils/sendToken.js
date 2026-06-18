835b0f976d0be9aa6fe3204cdcc0f646e4b85250
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-18 11:44:18 +0500

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,16 +1,20 @@
 
-const sendToken=async (user , statusCode , res)=>{
-    const token=user.getJwtToken();
-    const options={
-        expires : new Date(Date.now()+90*24*60*60*1000),
-        httpOnly:true,
-        sameSite: "lax", // Critical for cross-origin cookies
-        secure: false,    // Required if sameSite is "none"
-    }
-    res.status(statusCode).cookie("token" , token , options).json({
-        success:true , 
-        token , 
-        user,
-    })
-}
-module.exports=sendToken;
+// create troken and saving that in cookie
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+
+  // options for cookie
+  const isProd = process.env.NODE_ENV === "production";
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: isProd ? "none" : "lax",
+    secure: isProd,
+  };
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
+module.exports = sendToken;
