d75b7b30f0a9d11869e5bec76991d87f88240b8e
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-22 12:55:09 +0500

    Update Database.js

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,10 +1,18 @@
-const mongoose=require("mongoose");
-const connectDatabase = () => {
-  mongoose.connect(process.env.DB_URL).then((data) => {
-    console.log(`Mongo Db URL exist ${process.env.DB_URL}`);
+const mongoose = require("mongoose");
+
+const connectDatabase = async () => {
+  try {
+    console.log("DB_URL exists:", !!process.env.DB_URL);
+
+    const data = await mongoose.connect(process.env.DB_URL);
+
     console.log(
-      `Mongo db connected with server:${data.connection.host} successfully `
+      `Mongo db connected with server: ${data.connection.host} successfully`
     );
-  });
+  } catch (error) {
+    console.error("MongoDB Connection Error:");
+    console.error(error);
+  }
 };
+
 module.exports = connectDatabase;
